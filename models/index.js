6d869b77abc350b382053c0b7a940d9201c2a90e
janeishahu <janeishahu@aol.com>
2021-09-01 23:03:53 -0400

    set up connection to the DB

---
+++ models/index.js
@@ -0,0 +1,24 @@
+const Sequelize = require('sequelize')
+const AuthorsModel = require('./authors')
+const NovelsModel = require('/novels')
+const GenresModel = require('./novels')
+const NovelsGenresModel = require('novelsGenres')
+
+const connection = new Sequelize('books', 'greatReader', 'BoOk$!', {
+  host: 'localhost', dialect: 'mysql'
+})
+
+const Authors = AuthorsModel(connection, Sequelize)
+const Novels = NovelsModel(connection, Sequelize, Authors)
+const Genres = GenresModel(connection, Sequelize)
+const NovelsGenres = NovelsGenresModel(connection, Sequelize, Novels, Genres)
+
+
+
+module.exports = {
+  Authors,
+  Novels,
+  Genres,
+  NovelsGenres,
+}
+
