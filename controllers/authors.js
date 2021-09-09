d429331dd14184d5d940ac08f8b00a973124acee
janeishahu <janeishahu@aol.com>
2021-09-09 18:29:49 -0400

    added try/catch for DB exception.

--- controllers/authors.js
+++ controllers/authors.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const models = require('../models')
 
 const getAllAuthors = async (request, response) => {
@@ -8,27 +9,31 @@ const getAllAuthors = async (request, response) => {
 
 
 const getAuthorBySearchTerm = async (req, res) => {
-  const { searchTerm } = req.params
+  try {
+    const { searchTerm } = req.params
 
-  const authorsName = await models.Authors.findOne({
-    where: {
-      [models.Op.or]: [
-        { id: searchTerm },
-        { lastName: { [models.Op.like]: `%${searchTerm}%` } },
-      ],
-    },
-    include: [
-      {
-        model: models.Novels,
-        include: [{ model: models.Genres }],
+    const authorsName = await models.Authors.findOne({
+      where: {
+        [models.Op.or]: [
+          { id: searchTerm },
+          { lastName: { [models.Op.like]: `%${searchTerm}%` } },
+        ],
       },
-    ],
+      include: [
+        {
+          model: models.Novels,
+          include: [{ model: models.Genres }],
+        },
+      ],
 
-  })
+    })
 
-  return authorsName
-    ? res.send(authorsName)
-    : res.sendStatus(404)
+    return authorsName
+      ? res.send(authorsName)
+      : res.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('Could not find entry. Please try again!')
+  }
 }
 
 
--- controllers/novels.js
+++ controllers/novels.js
@@ -1,3 +1,4 @@
+const { response } = require('express')
 const models = require('../models')
 
 const getAllNovelsWithAuthorsAndGenres = async (request, response) => {
@@ -29,23 +30,27 @@ const getAllNovelsWithAuthorsAndGenres = async (request, response) => {
 //   }
 // }
 const getNovelBySearchTerm = async (req, res) => {
-  const { searchTerm } = req.params
+  try {
+    const { searchTerm } = req.params
 
-  const novel = await models.Novels.findOne({
-    where: {
-      [models.Op.or]: [
-        { id: searchTerm },
-        { title: { [models.Op.like]: `%${searchTerm}%` } },
-      ],
-    },
-    include: [{ model: models.Authors },
-      { model: models.Genres },
-    ]
-  })
+    const novel = await models.Novels.findOne({
+      where: {
+        [models.Op.or]: [
+          { id: searchTerm },
+          { title: { [models.Op.like]: `%${searchTerm}%` } },
+        ],
+      },
+      include: [{ model: models.Authors },
+        { model: models.Genres },
+      ]
+    })
 
-  return novel
-    ? res.send(novel)
-    : res.sendStatus(404)
+    return novel
+      ? res.send(novel)
+      : res.sendStatus(404)
+  } catch (error) {
+    return response.status(500).send('Could not find entry. Please try again!')
+  }
 }
 
 module.exports = { getAllNovelsWithAuthorsAndGenres, getNovelBySearchTerm }
